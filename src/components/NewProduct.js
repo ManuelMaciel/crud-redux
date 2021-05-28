b2493056d074909ac9dd6f6f3f69020d647b50b9
ManuelMaciel <manuelmaciel7001@hotmail.com>
2021-05-28 17:45:27 -0400

    added product syle

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,43 +1,45 @@
-import React from 'react';
+import React from "react";
 
 const NewProduct = () => {
-  return (  
-    <div className='row justify-content-center'>
-      <div className='col-md-8'>
-        <div className='card'>
-          <div className='card-body'>
-            <div className='text-center mb-4 font-weight-bold'>
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <div className="text-center mb-4 font-weight-bold">
               Add new product
             </div>
             <form>
-              <div className='form-group'>
+              <div className="form-group">
                 <label>Product Name</label>
-                <input 
-                  type='text'
-                  className='form-control'
-                  placeholder='Product Name'
-                  name='name'
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Product Name"
+                  name="name"
                 />
               </div>
-              <div className='form-group'>
+              <div className="form-group">
                 <label>Product Price</label>
-                <input 
-                  type='text'
-                  className='form-control'
-                  placeholder='Product Price'
-                  name='name'
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Product Price"
+                  name="name"
                 />
               </div>
-              <button 
-                type='submit'
-                className='btn btn-primary font-weight-bold text-uppercase d-block w-100'
-              >Add Product</button>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Add Product
+              </button>
             </form>
           </div>
         </div>
       </div>
     </div>
   );
-}
- 
+};
+
 export default NewProduct;
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Products = () => {
   return (  
-    <h1>From Products.js</h1>
+    <>
+      <h2 className='text-center my-5'>Product List</h2>
+
+      <table className='table table-striped'>
+        <thead className='bg-primary table-dark'>
+          <tr>
+            <th scope='col'>Name</th>
+            <th scope='col'>Price</th>
+            <th scope='col'>Actions</th>
+          </tr>
+        </thead>
+      </table>
+    </>
   );
 }
  
