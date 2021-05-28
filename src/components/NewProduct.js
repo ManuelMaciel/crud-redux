6daf4598c58614caa0195fa4d83c6e4b2635ec5d
ManuelMaciel <manuelmaciel7001@hotmail.com>
2021-05-28 17:31:50 -0400

    added sytles to newProduct component

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -2,7 +2,41 @@ import React from 'react';
 
 const NewProduct = () => {
   return (  
-    <h1>From NewProduct.js</h1>
+    <div className='row justify-content-center'>
+      <div className='col-md-8'>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='text-center mb-4 font-weight-bold'>
+              Add new product
+            </div>
+            <form>
+              <div className='form-group'>
+                <label>Product Name</label>
+                <input 
+                  type='text'
+                  className='form-control'
+                  placeholder='Product Name'
+                  name='name'
+                />
+              </div>
+              <div className='form-group'>
+                <label>Product Price</label>
+                <input 
+                  type='text'
+                  className='form-control'
+                  placeholder='Product Price'
+                  name='name'
+                />
+              </div>
+              <button 
+                type='submit'
+                className='btn btn-primary font-weight-bold text-uppercase d-block w-100'
+              >Add Product</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
  
